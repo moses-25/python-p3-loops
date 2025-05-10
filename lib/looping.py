82e87784e78ed813fabdf33e62cc0920efee6b6a
Mosesotieno-ai <mosesotieno8363@gmail.com>
2025-05-10 15:16:07 +0300

    Remove placeholder comment from fizzbuzz function

--- lib/looping.py
+++ lib/looping.py
@@ -16,7 +16,6 @@ def square_integers(int_list):
     pass
 
 def fizzbuzz():
-    # code goes here!
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             print("FizzBuzz")
